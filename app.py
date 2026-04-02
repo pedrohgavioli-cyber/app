66ccc0bc38352dd45c2cffddcbf6f45a85032043
Pedro Gavioli <pedrohgavioli@gmail.com>
2026-04-02 15:08:18 -0300

    ajustando o código do app

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ import streamlit as st
 
 st.header('Jogando uma moeda')
 
-st.write('Ainda não é um aplicativo funcional. Em construção.')
+num_tenativas = st.slider('Número de tentativas', min_value=1, max_value=1000, value=100)
+start = st.button('Iniciar')
+
+if start:
+    st.write(f'Jogando a moeda {num_tenativas} vezes...')
+
+st.write('em breve, resultados serão exibidos aqui!')
