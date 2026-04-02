e05fb36d03e779754e939fd1e61aba263b0ee1d3
Pedro Gavioli <pedrohgavioli@gmail.com>
2026-04-02 15:21:20 -0300

    atalizado

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 import streamlit as st
-
+import scipy.stats as sci
+import time
 st.header('Jogando uma moeda')
+
+graf = st.line_chart([0.5])
+def jogar_moeda(n):
+    outcumes = sci.bernoulli.rvs(0.5, size=n)
+
+    mean = None
+    outcume_no = 0
+    outcume_1_count = 0
+    for i in outcumes:
+        outcume_no += 1
+        if i == 1:
+            outcume_1_count += 1
+        mean = outcume_1_count / outcume_no
+        graf.add_rows([mean])
+        time.sleep(0.05)
+    return mean
+
 
 num_tenativas = st.slider('Número de tentativas', min_value=1, max_value=1000, value=100)
 start = st.button('Iniciar')
@@ -8,4 +26,3 @@ start = st.button('Iniciar')
 if start:
     st.write(f'Jogando a moeda {num_tenativas} vezes...')
 
-st.write('em breve, resultados serão exibidos aqui!')
